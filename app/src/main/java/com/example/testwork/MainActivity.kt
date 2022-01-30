eec540827b2c19fb106acc12d0709c4278947e8c
vyakhirev <vmu-ace@ya,ru>
2022-01-30 12:43:34 +0300

    2

--- app/src/main/java/com/example/testwork/MainActivity.kt
+++ app/src/main/java/com/example/testwork/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    val i=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
